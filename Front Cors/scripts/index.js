e69db9650f439d3c1de390acebf5ab0ffca17397
Latitarg <pabloalejandrodelaiglesia@gmail.com>
2023-09-01 22:47:30 -0300

    cors modificado

--- Front Cors/scripts/index.js
+++ Front Cors/scripts/index.js
@@ -25,6 +25,7 @@ function insertarDatos(datos){
         const elementoTabla3 = document.createElement("td");
         const elementoTabla4 = document.createElement("td");
         const elementoTabla5 = document.createElement("td");
+        const elementoTabla6 = document.createElement("td");
         // ID
         const id = datos[index].id;
         elementoTabla1.textContent=id;
@@ -40,11 +41,40 @@ function insertarDatos(datos){
         //ciudad
         const ciudad = datos[index].ciudad;
         elementoTabla5.textContent=ciudad;
+        // Boton
+        let elementoButton = document.createElement("button");
+        elementoButton.textContent = "Consultar";
+        elementoButton.class =  "btn btn-primary";
+        elementoButton.type = "button";
+        // elementoButton[index].addEventListener("onclick",consultar(id));
+        elementoButton.addEventListener("click", function(){
+            consultar(id);
+         });
+        elementoTabla6.appendChild(elementoButton);
         filaTabla.appendChild(elementoTabla1);
         filaTabla.appendChild(elementoTabla2);
         filaTabla.appendChild(elementoTabla3);
         filaTabla.appendChild(elementoTabla4);
         filaTabla.appendChild(elementoTabla5);
+        filaTabla.appendChild(elementoTabla6);
         cuerpoTabla.appendChild(filaTabla);
     }
+}
+
+function consultar(numero) {
+    fetch(`http://127.0.0.10:3000/data/${numero}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response body as JSON
+    })
+    .then(data => {
+        // Aquí puedes trabajar con los datos obtenidos
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+
 }
