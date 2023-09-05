6d3cfde31dc05acf9bd6c5d62a5c666380cfe5b9
Latitarg <pabloalejandrodelaiglesia@gmail.com>
2023-09-05 17:25:12 -0300

    Actualizacion del TATETI

---
+++ TaTeTi/scripts/start.js
@@ -0,0 +1,5 @@
+document.querySelector("#loading").style.display = "none";
+document.querySelector("#tablero").style.display = "none";
+document.querySelector("#nombre-rival").style.display = "none";
+document.querySelector("#turno-de").style.display = "none";
+document.querySelector("#nombre-rival").style.display = "none";
